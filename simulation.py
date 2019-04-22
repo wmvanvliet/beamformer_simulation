5ea8e214c9aab62ff9f5988cd32a43ea3585680e
ckiefer0 <c.m.k@gmx.net>
2019-04-22 16:38:32 +0200

    fixed dimensional mismatch in simulate_raw()

--- simulation.py
+++ simulation.py
@@ -11,13 +11,13 @@ data_path = sample.data_path()
 
 raw_fname = op.join(data_path, 'MEG/sample/sample_audvis_raw.fif')
 bem_fname = op.join(data_path, 'subjects/sample/bem/sample-5120-5120-5120-bem-sol.fif')
-src_fname = op.join(data_path, 'subjects/sample/bem/sample-oct-6-src.fif')
+src_fname = op.join(data_path, 'subjects/sample/bem/sample-oct-6-orig-src.fif')
 fwd_fname = op.join(data_path, 'MEG/sample/sample_audvis-meg-eeg-oct-6-fwd.fif')
 trans_fname = op.join(data_path, 'MEG/sample/sample_audvis_raw-trans.fif')
 
 info = mne.io.read_info(raw_fname)
-src = mne.read_source_spaces(src_fname)
 fwd = mne.read_forward_solution(fwd_fname)
+src = fwd['src']
 
 n_dipoles = 1
 times = np.arange(0, info['sfreq']) / info['sfreq']
@@ -27,11 +27,10 @@ stc = mne.simulation.simulate_sparse_stc(src, n_dipoles, times, data_fun=generat
 
 data = np.asarray([generate_signal(times, freq=10)])
 
-possible_vertices = src[1]['vertno']
+lh_vertno = src[0]['vertno']
+rh_vertno = src[1]['vertno']
 
-vertices = [np.array([]), np.array([possible_vertices[0]])]
-
-#vertices = stc.vertices
+vertices = [np.array([], dtype=np.int64), np.array([rh_vertno[0]], dtype=np.int64)]
 
 stc = mne.SourceEstimate(data=data, vertices=vertices, tmin=0,
                          tstep=1/info['sfreq'], subject='sample')
