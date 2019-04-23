cf7dfead3f30540e32a0bb9a1120b77b081ce494
Marijn van Vliet <w.m.vanvliet@gmail.com>
2019-04-23 17:38:29 +0200

    Move print statement

--- dics_beamformer.py
+++ dics_beamformer.py
@@ -55,10 +55,10 @@ for setting in settings:
         dip_true = make_dipole(stc_signal, fwd['src'])
         dip_est = make_dipole(stc, fwd['src'])
         dist = np.linalg.norm(dip_true.pos - dip_est.pos)
-        print(setting, dist)
     except Exception as e:
         print(e)
         dist = np.nan
+    print(setting, dist)
 
     dists.append(dist)
 
