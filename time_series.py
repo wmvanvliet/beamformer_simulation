6f1cfc07f7972d9ff8a84d204fbd64283518194c
ckiefer0 <c.m.k@gmx.net>
2019-10-18 16:12:25 +0200

    first implementation of simulate_raw_vol()

--- time_series.py
+++ time_series.py
@@ -4,9 +4,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mne.simulation import simulate_sparse_stc, simulate_raw as simulate_raw_mne
 from scipy.signal import butter, filtfilt
+from matplotlib import pyplot as plt
 
 import config
-from utils import add_stcs, set_directory
+from utils import add_stcs, add_volume_stcs, set_directory
+from utils import add_text_next_to_xlabel, random_three_vector
+
+from datetime import datetime
 
 
 def generate_signal(times, freq=10., n_trial=2, phase_lock=False):
@@ -238,3 +242,188 @@ def create_epochs(raw, trial_length, n_trials, fn_simulated_epochs=None, fn_repo
             report.save(fn_report_html, overwrite=True, open_browser=False)
 
     return epochs
+
+
+def simulate_raw_vol(info, src, fwd, bem, trans, signal_vertex, signal_freq,
+                    trial_length, n_trials, noise_multiplier, random_state, n_noise_dipoles,
+                    er_raw, fn_fwd_discrete, fn_stc_signal=None, fn_simulated_raw=None,
+                    fn_report_h5=None):
+    """
+    Simulate raw time courses for a single dipole with frequency
+    given by signal_freq. In each label a noise dipole is placed.
+
+    Parameters:
+    -----------
+    info : instance of Info | instance of Raw
+        The channel information to use for simulation.
+    src : instance of mne.SourceSpaces
+        The source space for which the raw instance is computed.
+    forward : instance of mne.Forward
+        The forward operator to use.
+    signal_vertex : int
+        The vertex where signal dipole is placed.
+    signal_hemi : 0 or 1
+        The signal dipole is placed in the left (0) or right (1) hemisphere.
+    signal_freq : float
+        The frequency of the signal.
+    trial_length : float
+        Length of a single trial in samples.
+    n_trials : int
+        Number of trials to create.
+    noise_multiplier : float
+        Multiplier for the noise dipoles. For noise_multiplier equal to one
+        the signal and noise dipoles have the same magnitude.
+    random_state : None | int | instance of RandomState
+        If random_state is an int, it will be used as a seed for RandomState.
+        If None, the seed will be obtained from the operating system (see
+        RandomState for details). Default is None.
+    labels : None | list of Label
+        The labels. The default is None, otherwise its size must be n_dipoles.
+    er_raw : instance of Raw
+        Empty room measurement to be used as sensor noise.
+    fn_stc_signal : None | string
+        Path where the signal source time courses are to be saved. If None the file is not saved.
+    fn_simulated_raw : None | string
+        Path where the raw data is to be saved. If None the file is not saved.
+    fn_report_h5 : None | string
+        Path where the .h5 file for the report is to be saved.
+
+    Returns:
+    --------
+    raw : instance of Raw
+        Simulated raw file.
+    stc_signal : instance of SourceEstimate
+        Source time courses of the signal.
+    """
+
+    times = np.arange(0, trial_length * info['sfreq']) / info['sfreq']
+    # there is only one volume source space
+    vertno = src[0]['vertno']
+
+    ###############################################################################
+    # Construct source space normals as random tangential vectors
+    ###############################################################################
+
+    rr = src[0]['rr']
+    com = rr.mean(axis=0)  # center of mass
+
+    # get vectors pointing from center of mass to voxels
+    radial = rr - com
+    rnd_vectors = np.array([random_three_vector() for i in range(rr.shape[0])])
+    tangential = np.cross(radial, rnd_vectors)
+    # normalize to unit length
+    nn = (tangential.T * (1. / np.linalg.norm(tangential, axis=1))).T
+
+    ###############################################################################
+    # Simulate a single signal dipole source as signal
+    ###############################################################################
+
+    signal_vert = src[0]['vertno'][signal_vertex]
+    data = np.asarray([generate_signal(times, freq=signal_freq)])
+    vertices = np.array([signal_vert])
+    stc_signal = mne.VolSourceEstimate(data=data, vertices=vertices, tmin=0,
+                                       tstep=1 / info['sfreq'], subject='sample')
+    if fn_stc_signal is not None:
+        set_directory(op.dirname(fn_stc_signal))
+        stc_signal.save(fn_stc_signal)
+
+    ###############################################################################
+    # Create discrete source space based on voxels in volume source space
+    ###############################################################################
+
+    if not op.exists(fn_fwd_discrete):
+
+        pos = {'rr': rr, 'nn': nn}
+
+        # make discrete source space
+        src_disc = mne.setup_volume_source_space(subject='sample', pos=pos,
+                                                 mri=None, bem=bem)
+
+        # setup_volume_source_space sets coordinate frame to MRI
+        # but coordinates we supplied are in head frame -> set correctly
+        src_disc[0]['coord_frame'] = fwd['src'][0]['coord_frame']
+
+        # np.array_equal(fwd_sel['src'][0]['rr'], fwd['src'][0]['rr'][stc.vertices]) is True
+        # np.isclose(fwd_sel['src'][0]['nn'], fwd['src'][0]['nn'][stc.vertices]) is True for all entries
+
+        # TODO: test if bem=bem works or if it has to be bem=bem_fname
+        fwd_disc = mne.make_forward_solution(info, trans=trans, src=src_disc,
+                                             bem=bem, meg=True, eeg=False)
+
+        fwd_disc = mne.convert_forward_solution(fwd_disc, surf_ori=True,
+                                                force_fixed=True)
+
+        mne.write_forward_solution(fn_fwd_discrete, fwd_disc, overwrite=False)
+
+    else:
+        fwd_disc = mne.read_forward_solution(fn_fwd_discrete)
+
+    ###############################################################################
+    # Create trials of simulated data
+    ###############################################################################
+
+    # select n_noise_dipoles entries from rr and their corresponding entries from nn
+    poss_indices = np.arange(rr.shape[0])
+
+    raw_list = []
+
+    for i in range(n_trials):
+        # Simulate random noise dipoles
+        stc_noise = simulate_sparse_stc(src, n_noise_dipoles, times,
+                                        data_fun=generate_random,
+                                        random_state=random_state,
+                                        labels=None)
+
+        # Project to sensor space
+        stc = add_volume_stcs(stc_signal, noise_multiplier * stc_noise)
+
+        raw = simulate_raw_mne(info, stc, trans=None, src=None,
+                               bem=None, forward=fwd_disc, cov=None)
+
+        raw_list.append(raw)
+        print('%02d/%02d' % (i + 1, n_trials))
+
+    raw = mne.concatenate_raws(raw_list)
+
+    # Use empty room noise as sensor noise
+    raw_picks = mne.pick_types(raw.info, meg=True, eeg=False)
+    er_raw_picks = mne.pick_types(er_raw.info, meg=True, eeg=False)
+    raw._data[raw_picks] += er_raw._data[er_raw_picks, :len(raw.times)]
+
+    ###############################################################################
+    # Save everything
+    ###############################################################################
+
+    if fn_simulated_raw is not None:
+        set_directory(op.dirname(fn_simulated_raw))
+        raw.save(fn_simulated_raw, overwrite=True)
+
+    # Plot the simulated raw data in the report
+    if fn_report_h5 is not None:
+        set_directory(op.dirname(fn_report_h5))
+        fn_report_html = fn_report_h5.rsplit('.h5')[0] + '.html'
+
+        now = datetime.now()
+        with mne.open_report(fn_report_h5) as report:
+            fig = plt.figure()
+            plt.plot(times, generate_signal(times, freq=10))
+            plt.xlabel('Time (s)')
+            ax = fig.axes[0]
+
+            add_text_next_to_xlabel(fig, ax, now.strftime('%m/%d/%Y, %H:%M:%S'))
+
+            report.add_figs_to_section(fig, now.strftime('Signal time course'),
+                                       section='Sensor-level', replace=True)
+
+            fig = raw.plot()
+
+            # axis 1 contains the xlabel
+            ax = fig.axes[1]
+
+            add_text_next_to_xlabel(fig, ax, now.strftime('%m/%d/%Y, %H:%M:%S'))
+
+            report.add_figs_to_section(fig, now.strftime('Simulated raw'),
+                                       section='Sensor-level', replace=True)
+            report.save(fn_report_html, overwrite=True, open_browser=False)
+
+    return raw, stc_signal
