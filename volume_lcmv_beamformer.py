753961886d1772f5d60b1720ac14d4ac421ef720
ckiefer0 <c.m.k@gmx.net>
2019-10-21 17:09:01 +0200

    implemented simulation of raw data directly in volume_lcmv_beamformer.py

--- volume_lcmv_beamformer.py
+++ volume_lcmv_beamformer.py
@@ -1,4 +1,5 @@
 import mne
+import os.path as op
 from mne.beamformer import make_lcmv, apply_lcmv
 import numpy as np
 from itertools import product
@@ -8,23 +9,60 @@ import config
 from config import vfname
 from utils import make_dipole_volume, evaluate_stc_volume
 
-# Read in the simulated data
-stc_signal = mne.read_source_estimate(vfname.stc_signal(noise=config.noise, vertex=config.vertex))
-epochs = mne.read_epochs(vfname.simulated_epochs(noise=config.noise, vertex=config.vertex))
-fwd_disc = mne.read_forward_solution(vfname.fwd_discrete)
+from time_series import simulate_raw_vol, create_epochs
 
+fn_stc_signal = vfname.stc_signal(noise=config.noise, vertex=config.vertex)
+fn_simulated_raw = vfname.simulated_raw(noise=config.noise, vertex=config.vertex)
+fn_simulated_epochs = vfname.simulated_epochs(noise=config.noise, vertex=config.vertex)
+
+fn_report_h5 = vfname.report(noise=config.noise, vertex=config.vertex)
+
+###############################################################################
+# Simulate raw data and create epochs
+###############################################################################
+
+if op.exists(fn_stc_signal + '-lh.stc') and op.exists(fn_simulated_epochs):
+    print('load stc_signal')
+    stc_signal = mne.read_source_estimate(fn_stc_signal)
+    print('load epochs')
+    epochs = mne.read_epochs(fn_simulated_epochs)
+
+else:
+    print('simulate data')
+    info = mne.io.read_info(vfname.sample_raw)
+    info = mne.pick_info(info, mne.pick_types(info, meg=True, eeg=False))
+    fwd_disc_true = mne.read_forward_solution(vfname.fwd_discrete_true)
+    fwd_disc_true = mne.pick_types_forward(fwd_disc_true, meg=True, eeg=False)
+    er_raw = mne.io.read_raw_fif(vfname.ernoise, preload=True)
+
+    raw, stc_signal = simulate_raw_vol(info=info, fwd_disc_true=fwd_disc_true, signal_vertex=config.vertex,
+                                       signal_freq=config.signal_freq, trial_length=config.trial_length,
+                                       n_trials=config.n_trials, noise_multiplier=config.noise,
+                                       random_state=config.random, n_noise_dipoles=config.n_noise_dipoles_vol,
+                                       er_raw=er_raw, fn_stc_signal=fn_stc_signal, fn_simulated_raw=fn_simulated_raw,
+                                       fn_report_h5=fn_report_h5)
+
+    del info, fwd_disc_true, er_raw
+
+    epochs = create_epochs(raw, config.trial_length, config.n_trials,
+                           fn_simulated_epochs=fn_simulated_epochs,
+                           fn_report_h5=fn_report_h5)
+
+fwd_disc_man = mne.read_forward_solution(vfname.fwd_discrete_man)
+
+# TODO: test if this is actually necessary for a discrete volume source space
 # For pick_ori='normal', the fwd needs to be in surface orientation
-# TODO: this should not be necessary since convert_forward_solution has used during fwd_disc creation
-fwd_disc = mne.convert_forward_solution(fwd_disc, surf_ori=True)
+fwd_disc_man = mne.convert_forward_solution(fwd_disc_man, surf_ori=True)
 
-# The DICS beamformer currently only uses one sensor type
+# The LCMV beamformer currently only uses one sensor type
 epochs_grad = epochs.copy().pick_types(meg='grad')
 epochs_mag = epochs.copy().pick_types(meg='mag')
 epochs_joint = epochs.copy().pick_types(meg=True)
 
 # Make cov matrix
-cov = mne.compute_covariance(epochs)
-noise_cov = mne.compute_covariance(epochs, (0.7, 1.3))
+cov = mne.compute_covariance(epochs, method='shrunk')
+# TODO: using 0.7 - 1.3 here but 0 to 0.3 for surface
+noise_cov = mne.compute_covariance(epochs, tmin=0.7, tmax=1.3, method='shrunk')
 
 evoked_grad = epochs_grad.average()
 evoked_mag = epochs_mag.average()
@@ -64,21 +102,16 @@ for setting in settings:
         else:
             raise ValueError('Invalid sensor type: %s', sensor_type)
 
-        filters = make_lcmv(
-            evoked.info,
-            fwd_disc,
-            cov,
-            reg=reg,
-            pick_ori=pick_ori,
-            weight_norm=weight_norm,
-            noise_cov=noise_cov if use_noise_cov else None,
-            depth=depth
-        )
+        filters = make_lcmv(evoked.info, fwd_disc_man, cov, reg=reg,
+                            pick_ori=pick_ori, weight_norm=weight_norm,
+                            noise_cov=noise_cov if use_noise_cov else None,
+                            depth=depth)
+
         stc = apply_lcmv(evoked, filters)
 
         # Compute distance between true and estimated source
-        dip_true = make_dipole_volume(stc_signal, fwd_disc['src'])
-        dip_est = make_dipole_volume(stc, fwd_disc['src'])
+        dip_true = make_dipole_volume(stc_signal, fwd_disc_man['src'])
+        dip_est = make_dipole_volume(stc, fwd_disc_man['src'])
         dist = np.linalg.norm(dip_true.pos - dip_est.pos)
 
         # Fancy evaluation metric
