21cd0a3a86c9c73d76100e7e21ded7a3129d9852
ckiefer0 <c.m.k@gmx.net>
2019-11-05 11:05:25 +0100

    print more info when making forward solution

--- make_manual_forward_solution.py
+++ make_manual_forward_solution.py
@@ -47,6 +47,8 @@ vfwd_disc_true, vfwd_disc_man = make_discrete_forward_solutions(info, vrr, vbem,
 # Check coregistration error
 ###############################################################################
 
+# surface source space coregistration error
+
 trans_true_mri_to_head = mne.transforms._get_trans(trans_true_head_to_mri,
                                                    fro='mri', to='head')[0]
 for hemi in range(2):
@@ -59,7 +61,9 @@ for hemi in range(2):
 
     distances = np.linalg.norm(rr_true_mri_to_head_to_mri - rr_true_mri, axis=1)
 
-    print('Surface hemi %d: avg. distance' % hemi, distances.mean())
+    print('Surface hemi %d: avg. distance %.4f, n_vertno %d' % (hemi, distances.mean(), len(rr_true_mri)))
+
+# Volume source space coregistration error
 
 vrr_true_mri = vsrc_true_mri[0]['rr'][vsrc_true_mri[0]['vertno']]
 # Transform the source space from mri to head space with true trans file
@@ -68,11 +72,11 @@ vrr_true_mri_to_head = mne.transforms.apply_trans(trans_true_mri_to_head, vrr_tr
 vrr_true_mri_to_head_to_mri = mne.transforms.apply_trans(trans_man_head_to_mri, vrr_true_mri_to_head)
 
 distances = np.linalg.norm(vrr_true_mri_to_head_to_mri - vrr_true_mri, axis=1)
-print('Volume: avg. distance', distances.mean())
+print('Volume: avg. distance %.4f, n_vertno %d' % (distances.mean(), len(vrr_true_mri)))
 
 vrr_disc_true_mri = vsrc_true_mri[0]['rr'][vfwd_disc_man['src'][0]['vertno']]
 vrr_disc_true_head = vfwd_disc_true['src'][0]['rr'][vfwd_disc_true['src'][0]['vertno']]
 # Transform the source space from head space back to mri space with inverse manual trans file
 vrr_disc_true_head_to_mri = mne.transforms.apply_trans(trans_man_head_to_mri, vrr_disc_true_head)
 distances_disc = np.linalg.norm(vrr_disc_true_head_to_mri - vrr_disc_true_mri, axis=1)
-print('Discrete volume: avg. distance', distances_disc.mean())
+print('Discrete volume: avg. distance %.4f, n_vertno %d' % (distances_disc.mean(), len(vrr_disc_true_mri)))
