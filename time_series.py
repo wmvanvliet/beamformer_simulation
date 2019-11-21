06d0a938a007466938acf64c4ede6c89c610e9a9
ckiefer0 <c.m.k@gmx.net>
2019-11-21 11:41:49 +0100

    added simulate_raw_vol_two_sources()

--- time_series.py
+++ time_series.py
@@ -399,8 +399,9 @@ def simulate_raw_vol(info, fwd_disc_true, signal_vertex, signal_freq,
                      fn_stc_signal=None, fn_simulated_raw=None,
                      fn_report_h5=None):
     """
-    Simulate raw time courses for a single dipole with frequency
-    given by signal_freq. In each label a noise dipole is placed.
+    Simulate raw time courses for two dipoles with frequencies
+    given by signal_freq1 and signal_freq2. Noise dipoles are
+    placed randomly in the whole cortex.
 
     Parameters:
     -----------
@@ -466,6 +467,164 @@ def simulate_raw_vol(info, fwd_disc_true, signal_vertex, signal_freq,
 
     # select n_noise_dipoles entries from rr and their corresponding entries from nn
     raw_list = []
+
+    for i in range(n_trials):
+        # Simulate random noise dipoles
+        stc_noise = simulate_sparse_stc(src, n_noise_dipoles, times,
+                                        data_fun=generate_random,
+                                        random_state=random_state,
+                                        labels=None)
+
+        # Project to sensor space
+        stc = add_volume_stcs(stc_signal, noise_multiplier * stc_noise)
+
+        raw = simulate_raw_mne(info, stc, trans=None, src=None,
+                               bem=None, forward=fwd_disc_true, cov=None)
+
+        raw_list.append(raw)
+        print('%02d/%02d' % (i + 1, n_trials))
+
+    raw = mne.concatenate_raws(raw_list)
+
+    # Use empty room noise as sensor noise
+    raw_picks = mne.pick_types(raw.info, meg=True, eeg=False)
+    er_raw_picks = mne.pick_types(er_raw.info, meg=True, eeg=False)
+    raw._data[raw_picks] += er_raw._data[er_raw_picks, :len(raw.times)]
+
+    ###############################################################################
+    # Save everything
+    ###############################################################################
+
+    if fn_simulated_raw is not None:
+        set_directory(op.dirname(fn_simulated_raw))
+        raw.save(fn_simulated_raw, overwrite=True)
+
+    # Plot the simulated raw data in the report
+    if fn_report_h5 is not None:
+        set_directory(op.dirname(fn_report_h5))
+        fn_report_html = fn_report_h5.rsplit('.h5')[0] + '.html'
+
+        now = datetime.now()
+        with mne.open_report(fn_report_h5) as report:
+            fig = plt.figure()
+            plt.plot(times, generate_signal(times, freq=10))
+            plt.xlabel('Time (s)')
+
+            ax = fig.axes[0]
+            add_text_next_to_xlabel(fig, ax, now.strftime('%m/%d/%Y, %H:%M:%S'))
+
+            report.add_figs_to_section(fig, now.strftime('Signal time course'),
+                                       section='Sensor-level', replace=True)
+
+            fig = raw.plot()
+
+            # axis 1 contains the xlabel
+            ax = fig.axes[1]
+            add_text_next_to_xlabel(fig, ax, now.strftime('%m/%d/%Y, %H:%M:%S'))
+
+            report.add_figs_to_section(fig, now.strftime('Simulated raw'),
+                                       section='Sensor-level', replace=True)
+            report.save(fn_report_html, overwrite=True, open_browser=False)
+
+    return raw, stc_signal
+
+
+def simulate_raw_vol_two_sources(info, fwd_disc_true, signal_vertex1, signal_freq1,
+                                 signal_vertex2, signal_freq2, trial_length, n_trials,
+                                 noise_multiplier, random_state, n_noise_dipoles,
+                                 er_raw, fn_stc_signal1=None, fn_stc_signal2=None,
+                                 fn_simulated_raw=None, fn_report_h5=None):
+    """
+    Simulate raw time courses for two dipoles with frequencies
+    given by signal_freq1 and signal_freq2. Noise dipoles are
+    placed randomly in the whole cortex.
+
+    Parameters:
+    -----------
+    info : instance of Info | instance of Raw
+        The channel information to use for simulation.
+    fwd_disc_true : instance of mne.Forward
+        The forward operator for the discrete source space created with
+        the true transformation file.
+    signal_vertex1 : int
+        The vertex where the first signal dipole is placed.
+    signal_freq1 : float
+        The frequency of the first signal.
+    signal_vertex2 : int
+        The vertex where the second signal dipole is placed.
+    signal_freq2 : float
+        The frequency of the second signal.
+    trial_length : float
+        Length of a single trial in samples.
+    n_trials : int
+        Number of trials to create.
+    noise_multiplier : float
+        Multiplier for the noise dipoles. For noise_multiplier equal to one
+        the signal and noise dipoles have the same magnitude.
+    random_state : None | int | instance of RandomState
+        If random_state is an int, it will be used as a seed for RandomState.
+        If None, the seed will be obtained from the operating system (see
+        RandomState for details). Default is None.
+    n_noise_dipoles : int
+        The number of noise dipoles to place within the volume.
+    er_raw : instance of Raw
+        Empty room measurement to be used as sensor noise.
+    fn_stc_signal1 : None | string
+        Path where the first signal source time courses are to be saved. If None the file is not saved.
+    fn_stc_signal2 : None | string
+        Path where the second signal source time courses are to be saved. If None the file is not saved.
+    fn_simulated_raw : None | string
+        Path where the raw data is to be saved. If None the file is not saved.
+    fn_report_h5 : None | string
+        Path where the .h5 file for the report is to be saved.
+
+    Returns:
+    --------
+    raw : instance of Raw
+        Simulated raw file.
+    stc_signal : instance of SourceEstimate
+        Source time courses of the signal.
+    """
+
+    times = np.arange(0, trial_length * info['sfreq']) / info['sfreq']
+
+    # TODO: I think a discrete source space was used because mne.simulate_raw did not take volume source spaces -> test
+    src = fwd_disc_true['src']
+    ###############################################################################
+    # Simulate a single signal dipole source as signal at vertex 1
+    ###############################################################################
+
+    signal_vert1 = src[0]['vertno'][signal_vertex1]
+    data = np.asarray([generate_signal(times, freq=signal_freq1)])
+    vertices = np.array([signal_vert1])
+    stc_signal1 = mne.VolSourceEstimate(data=data, vertices=vertices, tmin=0,
+                                        tstep=1 / info['sfreq'], subject='sample')
+    if fn_stc_signal1 is not None:
+        set_directory(op.dirname(fn_stc_signal1))
+        stc_signal1.save(fn_stc_signal1)
+
+    ###############################################################################
+    # Simulate a single signal dipole source as signal at vertex 2
+    ###############################################################################
+
+    # TODO: I think a discrete source space was used because mne.simulate_raw did not take volume source spaces -> test
+    signal_vert2 = src[0]['vertno'][signal_vertex2]
+    data = np.asarray([generate_signal(times, freq=signal_freq2)])
+    vertices = np.array([signal_vert2])
+    stc_signal2 = mne.VolSourceEstimate(data=data, vertices=vertices, tmin=0,
+                                        tstep=1 / info['sfreq'], subject='sample')
+    if fn_stc_signal2 is not None:
+        set_directory(op.dirname(fn_stc_signal2))
+        stc_signal2.save(fn_stc_signal2)
+
+    ###############################################################################
+    # Create trials of simulated data
+    ###############################################################################
+
+    # select n_noise_dipoles entries from rr and their corresponding entries from nn
+    raw_list = []
+
+    stc_signal = add_stcs(stc_signal1, stc_signal2)
 
     for i in range(n_trials):
         # Simulate random noise dipoles
