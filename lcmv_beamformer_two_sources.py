b08f13a1edd059feecfb0fbb2bb2fb9e6cdd6111
ckiefer0 <c.m.k@gmx.net>
2019-11-21 16:08:56 +0100

    took settings loop out of compute_lcmv_beamformer_results_two_sources()

--- lcmv_beamformer_two_sources.py
+++ lcmv_beamformer_two_sources.py
@@ -1,5 +1,6 @@
 import mne
 import numpy as np
+from itertools import product
 
 import config
 from config import fname
@@ -10,6 +11,21 @@ from spatial_resolution import compute_lcmv_beamformer_results_two_sources, get_
 
 # TODO: maybe create a second separate report or make sure that nothing gets overwritten
 fn_report_h5 = fname.report(noise=config.noise, vertex=config.vertex, hemi=config.signal_hemi)
+fn_report_h5 = None
+
+###############################################################################
+# Compute the settings grid
+###############################################################################
+
+regs = [0.05, 0.1, 0.5]
+sensor_types = ['joint', 'grad', 'mag']
+pick_oris = [None, 'normal', 'max-power']
+weight_norms = ['unit-noise-gain', 'nai', None]
+use_noise_covs = [True, False]
+depths = [True, False]
+
+settings = list(product(regs, sensor_types, pick_oris, weight_norms,
+                        use_noise_covs, depths))
 
 ###############################################################################
 # Load data
@@ -35,12 +51,15 @@ fwd_man = mne.convert_forward_solution(fwd_man, surf_ori=True)
 
 nearest_neighbors, distances = get_nearest_neighbors(config.vertex, config.signal_hemi, src_true)
 
-correlation = []
+corr_list = []
 
-for nb_vertex, nb_dist in np.column_stack(nearest_neighbors, distances):
+for nb_vertex, nb_dist in np.column_stack((nearest_neighbors, distances))[:config.n_neighbors_max]:
+
+    # after column_stack nb_vertex is float
+    nb_vertex = int(nb_vertex)
 
     ###############################################################################
-    # Simulate raw data and create epochs
+    # Simulate raw data
     ###############################################################################
 
     raw, stc_signal1, stc_signal2 = simulate_raw_two_sources(info, src=src_true, fwd=fwd_true,
@@ -58,19 +77,53 @@ for nb_vertex, nb_dist in np.column_stack(nearest_neighbors, distances):
                                                              fn_stc_signal1=None, fn_stc_signal2=None,
                                                              fn_simulated_raw=None, fn_report_h5=fn_report_h5)
 
+    # TODO: delete if not needed
     stc_signal = add_stcs(stc_signal1, stc_signal2)
+
+    ###############################################################################
+    # Create epochs
+    ###############################################################################
 
     # TODO: make sure nothing important is overwritten in report
     epochs = create_epochs(raw, config.trial_length, config.n_trials,
                            fn_simulated_epochs=None, fn_report_h5=fn_report_h5)
 
+    epochs_grad = epochs.copy().pick_types(meg='grad')
+    epochs_mag = epochs.copy().pick_types(meg='mag')
+    epochs_joint = epochs.copy().pick_types(meg=True)
+
+    # Make cov matrix
+    # TODO: would it not be better to compute cov
+    cov = mne.compute_covariance(epochs, method='empirical')
+    noise_cov = mne.compute_covariance(epochs, tmin=None, tmax=0.3, method='empirical')
+
+    evoked_grad = epochs_grad.average()
+    evoked_mag = epochs_mag.average()
+    evoked_joint = epochs_joint.average()
+
     ###############################################################################
     # Compute LCMV beamformer results
     ###############################################################################
 
-    corrs = compute_lcmv_beamformer_results_two_sources(epochs, fwd_man, signal_vertex1=config.vertex,
-                                                        signal_vertex2=nb_vertex, signal_hemi=config.signal_hemi)
+    # TODO: better organization of results
+    corrs = []
+    for setting in settings:
+        reg, sensor_type, pick_ori, weight_norm, use_noise_cov, depth = setting
+        try:
+            if sensor_type == 'grad':
+                evoked = evoked_grad
+            elif sensor_type == 'mag':
+                evoked = evoked_mag
+            elif sensor_type == 'joint':
+                evoked = evoked_joint
+            else:
+                raise ValueError('Invalid sensor type: %s', sensor_type)
 
-    correlation.append([nb_dist, corrs.mean()])
+            corr = compute_lcmv_beamformer_results_two_sources(setting, evoked, cov, noise_cov, fwd_man,
+                                                               signal_vertex1=config.vertex, signal_vertex2=nb_vertex,
+                                                               signal_hemi=config.signal_hemi)
+            corrs.append([setting, corr])
+
+    corr_list.append([nb_dist, corrs])
 
 # TODO: save results in report
--- spatial_resolution.py
+++ spatial_resolution.py
@@ -11,72 +11,30 @@ def correlation(stc, signal_vertex1, signal_vertex2, signal_hemi):
     vertex1 = signal_vertex1 if signal_hemi == 0 else signal_vertex1 + stc.vertices[0].shape[0]
     vertex2 = signal_vertex2 if signal_hemi == 0 else signal_vertex2 + stc.vertices[0].shape[0]
 
-    return pearsonr(stc[vertex1], stc[vertex2])[0]
+    return pearsonr(stc.data[vertex1], stc.data[vertex2])[0]
 
 
-def compute_lcmv_beamformer_results_two_sources(epochs, fwd_man, signal_vertex1, signal_vertex2, signal_hemi):
+def compute_lcmv_beamformer_results_two_sources(setting, evoked, cov, noise_cov, fwd_man,
+                                                signal_vertex1, signal_vertex2, signal_hemi):
 
-    epochs_grad = epochs.copy().pick_types(meg='grad')
-    epochs_mag = epochs.copy().pick_types(meg='mag')
-    epochs_joint = epochs.copy().pick_types(meg=True)
+    reg, sensor_type, pick_ori, weight_norm, use_noise_cov, depth = setting
+    try:
+        filters = make_lcmv(evoked.info, fwd_man, cov, reg=reg,
+                            pick_ori=pick_ori, weight_norm=weight_norm,
+                            noise_cov=noise_cov if use_noise_cov else None,
+                            depth=depth)
 
-    # Make cov matrix
-    cov = mne.compute_covariance(epochs, method='empirical')
-    noise_cov = mne.compute_covariance(epochs, tmin=None, tmax=0.3, method='empirical')
+        stc = apply_lcmv(evoked, filters)
 
-    evoked_grad = epochs_grad.average()
-    evoked_mag = epochs_mag.average()
-    evoked_joint = epochs_joint.average()
+        corr = correlation(stc, signal_vertex1, signal_vertex2, signal_hemi)
 
-    # Compute the settings grid
-    # regs = [0.05, 0.1, 0.5]
-    # sensor_types = ['joint', 'grad', 'mag']
-    # pick_oris = [None, 'normal', 'max-power']
-    # weight_norms = ['unit-noise-gain', 'nai', None]
-    # use_noise_covs = [True, False]
-    # depths = [True, False]
-    regs = [0.05]
-    sensor_types = ['joint']
-    pick_oris = ['max-power']
-    weight_norms = [None]
-    use_noise_covs = [True]
-    depths = [True, False]
-    settings = list(product(regs, sensor_types, pick_oris, weight_norms,
-                            use_noise_covs, depths))
+    except Exception as e:
+        print(e)
+        corr = np.nan
 
-    # Compute LCMV beamformer with all possible settings
-    corrs = []
+    print(setting, corr)
 
-    for setting in settings:
-        reg, sensor_type, pick_ori, weight_norm, use_noise_cov, depth = setting
-        try:
-            if sensor_type == 'grad':
-                evoked = evoked_grad
-            elif sensor_type == 'mag':
-                evoked = evoked_mag
-            elif sensor_type == 'joint':
-                evoked = evoked_joint
-            else:
-                raise ValueError('Invalid sensor type: %s', sensor_type)
-
-            filters = make_lcmv(evoked.info, fwd_man, cov, reg=reg,
-                                pick_ori=pick_ori, weight_norm=weight_norm,
-                                noise_cov=noise_cov if use_noise_cov else None,
-                                depth=depth)
-
-            stc = apply_lcmv(evoked, filters)
-
-            corr = correlation(stc, signal_vertex1, signal_vertex2, signal_hemi)
-
-        except Exception as e:
-            print(e)
-            corr = np.nan
-
-        print(setting, corr)
-
-        corrs.append(corr)
-
-    return np.array(corrs)
+    return corr
 
 
 def get_nearest_neighbors(signal_vertex, signal_hemi, src):
