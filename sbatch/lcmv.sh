a84c6120034c6eeaa23218ba27e379883932cbc3
Marijn van Vliet <marijn.vanvliet@aalto.fi>
2020-01-10 21:24:35 +0200

    remove xvfb server from sbatch scripts

--- sbatch/lcmv.sh
+++ sbatch/lcmv.sh
@@ -9,7 +9,7 @@
 #SBATCH --job-name lcmv
 
 # Do the analysis for each vertex.
-#SBATCH --array=0-3765
+#SBATCH --array=0-3756
 
 #SBATCH --output=lcmv.out --open-mode=append
 
@@ -19,15 +19,10 @@ LOG_FILE=logs/lcmv.log
 VERTEX_NUMBER=$(printf "%04d" $SLURM_ARRAY_TASK_ID)
 
 # Load the python environment
-module load mesa
 module load anaconda3
 
 # Tell BLAS to only use a single thread
 export OMP_NUM_THREADS=1
-
-# Start a virtual framebuffer to render things to
-Xvfb :99 -screen 0 1400x900x24 -ac +extension GLX +render -noreset &
-export DISPLAY=:99.0
 
 # Run the script
 srun python ../lcmv.py -v $SLURM_ARRAY_TASK_ID -n 0.1 2>&1 | sed -e "s/^/$VERTEX_NUMBER:  /" >> $LOG_FILE
