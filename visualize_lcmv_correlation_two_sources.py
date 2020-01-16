6ff86205d315f86449ef943b81090587862dc9e7
ckiefer0 <c.m.k@gmx.net>
2020-01-16 09:59:51 +0200

    implemented new html footer

--- visualize_lcmv_correlation_two_sources.py
+++ visualize_lcmv_correlation_two_sources.py
@@ -73,7 +73,40 @@ html_header = '''
 
 html_footer = '''
         <script src="tablefilter/tablefilter.js"></script>
-        <script src="filter.js"></script>
+        <script>
+            var filtersConfig = {
+                base_path: 'tablefilter/',
+                col_0: 'checklist',
+                col_1: 'checklist',
+                col_2: 'checklist',
+                col_3: 'checklist',
+                col_4: 'checklist',
+                col_5: 'checklist',
+                col_6: 'checklist',
+                col_7: 'checklist',
+                col_8: 'none',
+                filters_row_index: 1,
+                enable_checklist_reset_filter: false,
+                alternate_rows: true,
+                col_types: [
+                    'number', 'string', 'string',
+                    'string', 'string', 'string',
+                    'string', 'string', 'image'
+                ],
+                col_widths: [
+                    '80px', '150px', '130px',
+                    '110px', '170px', '150px',
+                    '150px', '150px', '210px'
+                ]
+            };
+
+            var tf = new TableFilter('results', filtersConfig);
+            tf.init();
+
+            for (div of document.getElementsByClassName("div_checklist")) {
+                div.style.height = 100;
+            }
+        </script>
     </body>
 </html>
 '''
--- visualize_lcmv_localization_accuracy.py
+++ visualize_lcmv_localization_accuracy.py
@@ -1,4 +1,5 @@
 import os.path as op
+import warnings
 
 import mne
 import numpy as np
@@ -9,6 +10,8 @@ from tqdm import tqdm
 import config
 from config import fname
 from utils import set_directory
+
+warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 ###############################################################################
 # Load volume source space
