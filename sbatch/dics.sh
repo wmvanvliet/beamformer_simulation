264f1ca1ccea026eae8ab30096bd6ac15b9f751e
Marijn van Vliet <marijn.vanvliet@aalto.fi>
2020-02-21 12:50:30 +0200

    Some more memory too

--- sbatch/dics.sh
+++ sbatch/dics.sh
@@ -2,7 +2,7 @@
 
 # Make sure to request only the resources you really need to avoid cueing
 #SBATCH -t 2:00:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=8G
 #SBATCH -n 1
 
 # A name for the job
