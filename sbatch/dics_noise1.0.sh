2c898f15bce4b06bec4945f8f271cfb700f21080
Marijn van Vliet <marijn.vanvliet@aalto.fi>
2020-03-02 11:17:08 +0200

    Fix noise1.0 sbatch scripts

--- sbatch/dics_noise1.0.sh
+++ sbatch/dics_noise1.0.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Make sure to request only the resources you really need to avoid cueing
-#SBATCH -t 90:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH -t 2:00:00
+#SBATCH --mem-per-cpu=8G
 #SBATCH -n 1
 
 # A name for the job
