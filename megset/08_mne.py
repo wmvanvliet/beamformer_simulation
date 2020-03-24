cf359fc1f487fba826de51388650860d2fb5f326
Marijn van Vliet <w.m.vanvliet@gmail.com>
2020-03-24 13:12:15 +0200

    Set MNE peak time

--- megset/08_mne.py
+++ megset/08_mne.py
@@ -1,5 +1,6 @@
 import mne
 import argparse
+import numpy as np
 from config import fname
 
 # Handle command line arguments
@@ -16,7 +17,11 @@ inv = mne.minimum_norm.make_inverse_operator(epochs.info, fwd, noise_cov)
 stc = mne.minimum_norm.apply_inverse(epochs.average(), inv)
 stc.save(fname.stc_mne(subject=subject))
 
-stc_peak = abs(stc.copy().crop(0.035, 0.035).mean())
+# Find the time point that corresponds to the best dipole fit
+dip = mne.read_dipole(fname.ecd(subject=subject))
+peak_time = dip[int(np.argmax(dip.gof))].times[0]
+
+stc_peak = abs(stc.copy().crop(peak_time, peak_time).mean())
 stc_peak.save_as_volume(fname.nii_mne(subject=subject), src=fwd['src'])
 
 fig = stc.plot(initial_time=0.04, subject=fname.subject_id(subject=subject), subjects_dir=fname.subjects_dir, src=fwd['src'],
